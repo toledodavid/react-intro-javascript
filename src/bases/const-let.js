6289c9c32fc1633a593c8ab6fd20f202f071f83f
David Toledo <dtoledoz0207@gmail.com>
2020-10-13 17:19:27 -0500

    js-bases-const-let

---
+++ src/bases/const-let.js
@@ -0,0 +1,14 @@
+
+// let and const
+
+const name = 'David';
+const lastName = 'Toledo';
+
+let age = 26;
+
+if (true) {
+  let age = 89;
+  console.log(age);
+}
+
+console.log(name, lastName, age);
