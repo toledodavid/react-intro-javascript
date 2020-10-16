0823cd08e413beab512066f71d4758b19132bee0
David Toledo <dtoledoz0207@gmail.com>
2020-10-16 12:22:25 -0500

    js-bases-ternary-operator

---
+++ src/bases/12-ternary.js
@@ -0,0 +1,17 @@
+// console.log('Hello!');
+
+const active = true;
+
+// let message = '';
+
+// if (active) {
+//   message = 'Active';
+// } else {
+//   message = 'Inactive';
+// }
+
+//const message = (active) ? 'Active' : 'Inactive';
+
+const message = active && 'Active';
+
+console.log(message);
